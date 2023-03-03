b9ddbc02246bba7bed8200a0ee67a6b77eacede8
ali <ali.mozafari733@yahoo.com>
2023-03-03 13:21:46 +0330

    add js classlist toggle

--- Sidebar.js
+++ Sidebar.js
@@ -3,3 +3,8 @@ let navbar = document.getElementById("navbar");
 let bodypadd = document.getElementById("body");
 
 
+toggle.addEventListener("click", () => {
+  toggle.classList.toggle("rotate");
+  navbar.classList.toggle("show");
+  bodypadd.classList.toggle("expander");
+});
